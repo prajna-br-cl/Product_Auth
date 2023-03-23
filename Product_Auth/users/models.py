5764bb57558de56c550160144dd6b00d6e5e64bb
prajna-br-cl <116802527+prajna-br-cl@users.noreply.github.com>
2023-03-23 18:27:58 +0530

    Add files via upload

---
+++ Product_Auth/users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    likes = models.PositiveIntegerField(default=0)
---
+++ Product_Auth/users/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import RegisterView, LoginView, UserView, LogoutView, ProductViewSet
+
+urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
+    path('products', ProductViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('products/<str:pk>', ProductViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    }))
+]
